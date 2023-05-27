611e989ccdfdb82a77b1f322c8d501bc840ab7e7
Muhammad Arslan Ahmad <arsil721@gmail.com>
2023-05-27 17:28:50 +0500

    gym

---
+++ signin.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="signinS.css">
+    <link rel="stylesheet" href="style.css">
+    <title>signin</title>
+  </head>
+  <body>
+   
+  <div class="container-fluid">
+    <div class="container">
+    <br><br>
+    <div class="row">
+        <div class="col-md-4 col-sm-12 pb-5 pt-5">
+        <form>
+        <h2 class="text-center" style="font-size: 30px; color: orange;">Signin Into<span style="color: white;"> Your Accout</span></h2>
+                <br>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label text-white">Email address</label>
+                    <input type="email"  class="form-control" id="box" aria-describedby="emailHelp">
+                    
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label text-white">Password</label>
+                    <input type="password" class="form-control" id="box" >
+                </div>
+                
+                <div id="link">
+                <a style="text-decoration: none;" href="signup.php" target="_blank"><span style="color:orange;">Click Here For </span><span style="color:white;">Signup</span></a>
+
+                </div>
+                <br>
+                <div class="d-grid">
+                    <button type="submit" id="btn" class="btn btn-primary btn-block">SignIn</button>
+                </div>
+                
+            </form>
+
+        </div>
+    </div>
+    </div>
+  </div>
+   
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    
+  </body>
+</html>
---
+++ signup.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="signinS.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Signup</title>
+  </head>
+  <body>
+   
+  <div class="container-fluid">
+    <div class="container">
+    <br><br>
+    <div class="row">
+        <div class="col-md-4 col-sm-12 pb-5 pt-5">
+        <form>
+        <h2 class="text-center" style="font-size: 30px; color: orange;">Create<span style="color: white;"> Your Accout</span></h2>
+                <br>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label text-white">Email address</label>
+                    <input type="email"  class="form-control" id="box" aria-describedby="emailHelp">
+                    
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label text-white">Password</label>
+                    <input type="password" class="form-control" id="box" >
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label text-white">Confirm Password</label>
+                    <input type="password" class="form-control" id="box" >
+                </div>
+                
+                <div class="d-grid">
+                    <button type="submit" id="btn" class="btn btn-primary btn-block">SignUp</button>
+                </div>
+                
+            </form>
+
+        </div>
+    </div>
+    </div>
+  </div>
+   
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    
+  </body>
+</html>
